a69668d647131523ae4e34ea5f19dd422790d66b
msgamecoder <olamilekandamilaraa@gmail.com>
2025-04-18 21:11:29 +0100

    mxapi

--- mxgamecoder/mxconfig/mxapi.js
+++ mxgamecoder/mxconfig/mxapi.js
@@ -1,10 +1,10 @@
-const fetch = require('node-fetch'); // make sure to install this if needed
-
 const primary = "https://msworld.onrender.com";
 const fallback = "https://msworldd.onrender.com";
 
 async function checkAPI(url) {
     try {
+        // Dynamically import 'node-fetch'
+        const fetch = (await import('node-fetch')).default;
         const res = await fetch(url + "/", { method: "GET" });
         const text = await res.text();
         return res.ok && text.includes("MXWorld API");
