5ede4345033fce67e5a8c75c1b2ae2f974d41c10
msgamecoder <olamilekandamilaraa@gmail.com>
2025-05-14 14:21:48 +0100

    mxapi

--- mxcore.js
+++ mxcore.js
@@ -19,7 +19,10 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://127.0.0.1:5500', 'https://mxgamecoder.lovestoblog.com'],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
